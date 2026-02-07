f978998bc8b2b57f834e849e6e0d3e6cb9d91bc0
Hadoni00 <hiepdinh0604@gmail.com>
2026-02-07 09:37:12 +0700

    update

--- OOP/BAI3.py
+++ OOP/BAI3.py
@@ -5,13 +5,13 @@ s = input().strip()
 birth = datetime.strptime(s, "%Y-%m-%d")
 now = datetime.now()
 
-# a) số ngày đã sống
+# a) số ngày 
 print("So ngay da song:", (now - birth).days)
 
 # b) Unix timestamp ngày sinh
 print("Unix timestamp ngay sinh:", int(birth.timestamp()))
 
-# c) mốc 49 và 100 ngày (giả định mất 23/04/2100)
+# c) mốc 49 và 100 
 death = datetime(2100, 4, 23)
 
 day_49 = death + timedelta(days=49)
